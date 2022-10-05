846ebe4c01a2bda721160d53084e6066da499da2
Conor Higgins <wodowods@gmail.com>
2022-10-05 14:53:56 +0100

    exclude datasourceautoconfiguration

--- src/main/java/com/nology/cpg/CpgApplication.java
+++ src/main/java/com/nology/cpg/CpgApplication.java
@@ -2,12 +2,13 @@ package com.nology.cpg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CpgApplication {
 
 	public static void main(String[] args) {
